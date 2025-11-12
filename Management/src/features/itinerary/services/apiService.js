f54e434d9209fafe2b68437a4005ebb47d9f987f
sjrathnayaka <hasath30@gmail.com>
2025-11-13 00:01:01 +0530

    Revert "Merge branch 'develop' into lead-management"
    This reverts commit aa5ce2fd64ea0a056e53b7864ab5acd54a881e56, reversing
    changes made to f653192192f70b9393519d4d7e9c0c40bdd8210c.

--- Management/src/features/itinerary/services/apiService.js
+++ Management/src/features/itinerary/services/apiService.js
@@ -23,6 +23,11 @@ async function makeRequest(endpoint, options = {}) {
   };
 
   try {
+    console.log(`[API] ${options.method || 'GET'} ${endpoint}`);
+    if (options.body) {
+      console.log(`[API Request Body]:`, JSON.parse(options.body));
+    }
+
     const response = await fetch(url, config);
     const data = await response.json();
 
