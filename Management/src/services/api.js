c0e22a184029ec630124bb8c673bbc56b5dd7e07
NANAYAKKARA A.H.M. <92136486+harithmaduranga@users.noreply.github.com>
2026-02-10 15:04:46 +0530

    Update API base URL for production environment

--- Management/src/services/api.js
+++ Management/src/services/api.js
@@ -1,5 +1,5 @@
 const API_BASE_URL =
-  import.meta.env.VITE_API_URL || "https://api.harshkumar.space/api/v1";
+  import.meta.env.VITE_API_URL || "https://api.lushtravelcloud.com/api/v1";
   // import.meta.env.VITE_API_URL || "http://localhost:5000/api/v1";
 
 class ApiService {
